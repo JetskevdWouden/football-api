f883ed10d499083131d9372dc05676635adf741a
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-26 17:54:47 +0200

    auth middleware

--- auth/router.js
+++ auth/router.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const { toJWT, toData } = require('./jwt')
 const bcrypt = require('bcrypt');
 const User = require('../user/model')
+const auth = require('./middleware')
 const router = new Router();
 
 //define end points here
@@ -57,28 +58,37 @@ router.post('/logins', (req, res, next) => {        //expect an email and passwo
 })
 
 //secret endpoint
-router.get('/secret-endpoint', (req, res) => {
-    const auth = req.headers.authorization && req.headers.authorization.split(' ');
-    if (auth && auth[0] === 'Bearer' && auth[1]) {
-        try {
-            const data = toData(auth[1])
-            res.send({
-                message: 'Thanks for visiting the secret endpoint.',
-                data
-            })
-        }
-        catch (error) {
-            res.status(400).send({
-                message: `Error ${error.name}: ${error.message}`,
-            })
-        }
-    } else {
-        res
-            .status(401)
-            .send({
-                message: 'Please suply some valid credentials.Thankssss'
-            })
-    }
+// router.get('/secret-endpoint', (req, res) => {
+//     const auth = req.headers.authorization && req.headers.authorization.split(' ');
+//     if (auth && auth[0] === 'Bearer' && auth[1]) {
+//         try {
+//             const data = toData(auth[1])
+//             res.send({
+//                 message: 'Thanks for visiting the secret endpoint.',
+//                 data
+//             })
+//         }
+//         catch (error) {
+//             res.status(400).send({
+//                 message: `Error ${error.name}: ${error.message}`,
+//             })
+//         }
+//     } else {
+//         res
+//             .status(401)
+//             .send({
+//                 message: 'Please suply some valid credentials.Thankssss'
+//             })
+//     }
+// })
+
+//with auth(middleware) instead of ^^^
+
+router.get('/secret-endpoint', auth, (req, res) => {
+    res
+        .send({
+            message: `Thanks for visiting the secret endpoint ${req.user.email}.`
+        })
 })
 
 module.exports = router;
