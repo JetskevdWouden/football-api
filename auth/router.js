8ad80b59f6bc2de33412f0a6b79d7f86b7cace2f
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-26 17:38:27 +0200

    password and email verification

--- auth/router.js
+++ auth/router.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const { toJWT, toData } = require('./jwt')
+const bcrypt = require('bcrypt');
+const User = require('../user/model')
 const router = new Router();
 
 //define end points here
@@ -7,9 +9,43 @@ router.post('/logins', (req, res, next) => {        //expect an email and passwo
     const email = req.body.email
     const password = req.body.password
     if (email && password) {
-        res
-            .send({
-                jwt: toJWT({ userId: 1 })               //hard coded because no real database
+        // 1. find user based on email address
+        User
+            .findOne({
+                where: {
+                    email: email
+                }
+            })
+            .then(entity => {
+                if (!entity) {
+                    res
+                        .status(400)
+                        .send({
+                            message: 'User with that email does not exist'
+                        })
+                }
+        // 2. use bcrypt.compareSync to check the password against the stored hash
+                if (bcrypt.compareSync(password, entity.password)) {
+        // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+                    res
+                        .send({
+                            jwt: toJWT({ userId: entity.id })
+                        })
+                } else {
+                    res
+                        .status(400)
+                        .send({
+                            message: 'Password was incorrect'
+                        })
+                }
+            })
+            .catch(err => {
+                console.error(err)
+                res
+                    .status(500)
+                    .send({
+                        message: 'Something went wrong'
+                    })
             })
     } else {
         res
